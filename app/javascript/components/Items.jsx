0c58fb71bd015b5e280bed98b5b12c0fcb0b38ab
nickwohnhas <npw7887@gmail.com>
2020-01-04 21:25:41 -0500

    Remove console log

--- app/javascript/components/Items.jsx
+++ app/javascript/components/Items.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Item from './Item.jsx';
 
 const Items = ({items}) => {
-  console.log(items)
+
   return (
     <div className="user-list">
       {items.map(item => <Item key={item.id} name={item.name}/>)}
