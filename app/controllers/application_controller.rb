658a48caa38b0b2fa9c157cc526e75eccdd4dffd
nickwohnhas <npw7887@gmail.com>
2020-09-06 18:50:57 -0400

    Move recipe count to application controller
    I added some error handling to account for when a user is signed out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
-
+  before_action :cart_item_count
   def ensure_user_has_cart
     unless current_user.recipe_cart
       RecipeCart.create! user: current_user
     end
+  end
+
+  def cart_item_count
+    begin
+      @cart_count = current_user.recipe_cart.items.count
+    rescue
+      @cart_count =  0 
+    end  
   end
 end
