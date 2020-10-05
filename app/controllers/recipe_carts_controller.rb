0c9c61eb839045f2a4814a71758cca9bb3c6f29d
nickwohnhas <npw7887@gmail.com>
2020-10-04 22:56:43 -0400

    Re-design 'Find Recipes' feature
    The user will no longer search for recipes based on what they already have, but now they have the freedom to search for any recipe

--- app/controllers/recipe_carts_controller.rb
+++ app/controllers/recipe_carts_controller.rb
@@ -9,17 +9,20 @@ class RecipeCartsController < ApplicationController
     group = Group.find(params[:group_id])
     items = Item.where(id: params[:group][:item_ids])
     current_user.recipe_cart.items << items
-     if current_user.recipe_cart.save
-        redirect_to group_path(group), notice: "Successfully added items to cart!"
-     else
+    if current_user.recipe_cart.save
+      redirect_to group_path(group), notice: "Successfully added items to cart!"
+    else
       redirect_to group_path(group), notice: "Failed to add items"
-     end
+    end
   end
 
   def recipes
-    food_items = Item.where(recipe_cart: current_user.recipe_cart).pluck(:name).join(",")
-    response = Faraday.get "https://api.spoonacular.com/recipes/findByIngredients?apiKey=#{ENV["SPOONTACULAR_API_KEY"]}&ingredients=#{food_items}"
-    @items = JSON.parse(response.body)
+  end
+
+
+  def search_recipes
+    # query api
+    # render json of recipes
   end
 
   def remove_item
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get '/recipe_cart/all_items', to: 'recipe_carts#all_items'
   post '/recipe_cart/add_items', to: 'recipe_carts#add_items'
+  post 'recipe_cart/search_recipes', to: 'recipe_carts#search_recipes'
   get '/recipe_cart/recipes', to: 'recipe_carts#recipes'
   delete '/recipe_cart/remove_item/:id', to: 'recipe_carts#remove_item'
 
